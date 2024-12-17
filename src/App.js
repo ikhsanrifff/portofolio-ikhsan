83afddf8754e64af89e80fe5ec4f7b0626e1cfe9
Ikhsan Rifansyah <ikhsanrifansyah.280206@gmail.com>
2024-12-17 09:54:20 +0700

    *add profile n fix title

--- src/App.js
+++ src/App.js
@@ -14,28 +14,36 @@ function App() {
   return (
     <div className="App">
       <header>
-      <nav className="navbar">
-        <div className="logo">Ikhsan Rifansyah</div>
-        <ul className={isOpen ? "nav-links open" : "nav-links"}>
-          <div className="close" onClick={exitMenu}>
-          <div>X</div>
+        <nav className="navbar">
+          <div className="logo">Ikhsan Rifansyah</div>
+          <ul className={isOpen ? "nav-links open" : "nav-links"}>
+            <div className="close" onClick={exitMenu}>
+              <div>X</div>
+            </div>
+            <li><a href="/App.js">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/services">Services</a></li>
+            <li><a href="/contact">Contact</a></li>
+          </ul>
+          <div className="burger" onClick={toggleMenu}>
+            <div className="line1"></div>
+            <div className="line2"></div>
+            <div className="line3"></div>
           </div>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-        <div className="burger" onClick={toggleMenu}>
-          <div className="line1"></div>
-          <div className="line2"></div>
-          <div className="line3"></div>
-        </div>
-      </nav>
-    </header>
+        </nav>
+      </header>
 
       <section>
-        <div id='profile' className='profile'>
-          <div>Profil</div>
+        <div id='me' className='me'>
+          <div className="profile">
+            <h4>
+              Profil
+            </h4>
+            <p>
+              Perkenalkan nama saya Ikhsan Rifansyah, lulusan SMK Negeri 4 Bandung dari jurusan Rekayasa Perangkat Lunak.
+              Memiliki Keahlian di bidang komputer selama masa sekolah dan berdasarkan pengalaman magang sebagai Frontend Web Developer.
+            </p>
+          </div>
           <div>Pengalaman</div>
           <div>Pendidikan</div>
           <div>Keahlian</div>
